8180a5583776dede4492fe2b0e91b9ab8d8b51a7
PiadonyFX <duterronp@gmail.com>
2026-08-11 12:25:45 -0400

    fixed toast

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/globals.css";
-import "./styles/components/toast.css";
+import "./styles/components/Toast.css";
 import "./styles/components/backButton.css";
 
 createRoot(document.getElementById('root')).render(
